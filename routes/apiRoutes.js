75af096dab6b0c3410ff40765a1fcbb9ef833866
hilbo <hardimanhk@gmail.com>
2018-11-27 14:56:11 -0500

    corrected database build

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,24 +2,25 @@ var db = require("../models");
 var databaseFill = require("../database/database-build");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all ingredients
+  app.get("/api/ingredients", function(req, res) {
+    db.ingredient.findAll({}).then(function(ingredients) {
+      res.json(ingredients);
     });
   });
 
-  // Create a new example
+  // Create a new recipe
   app.post("/api/recipes", function(req, res) {
-    db.Example.create(req.body).then(function(newRecipe) {
+    databaseFill().then(function(newRecipe) {
       res.json(newRecipe);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Delete a recipe by id
+  app.delete("/api/recipes/:id", function(req, res) {
+    db.recipe.destroy({ where: { id: req.params.id } }).then(function(dbRecipe) {
+      res.json(dbRecipe);
+      // do we need to delete all lines in join where this recipe appears?
     });
   });
 };

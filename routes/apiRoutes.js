d96db7d93eff7fa6ed685635b5d27f2c187d5d4d
hilbo <hardimanhk@gmail.com>
2018-11-27 20:41:36 -0500

    log

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all ingredients
-=======
+
   app.get("/api/ingredients", function(req, res) {
     db.ingredient.findAll({}).then(function(ingredients) {
       res.json(ingredients);

efd8a1aaedb60fb8d3f0d30f8305481e823a1980
hilbo <hardimanhk@gmail.com>
2018-11-27 20:46:24 -0500

    done?

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,23 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all ingredients
 
-  app.get("/api/ingredients", function(req, res) {
-    db.ingredient.findAll({}).then(function(ingredients) {
+  app.get("/api/ingredients", function (req, res) {
+    db.ingredient.findAll({}).then(function (ingredients) {
       res.json(ingredients);
     });
   });
 
   // Create a new recipe
-  app.post("/api/recipes", function(req, res) {
-    databaseFill().then(function(newRecipe) {
+  app.post("/api/recipes", function (req, res) {
+    databaseFill().then(function (newRecipe) {
       res.json(newRecipe);
     });
   });
-  
-   //using the selected ingredients, find all recipes that contain at least the selected ingredient
-  app.post("/api/search/", function(req, res){
+
+  //using the selected ingredients, find all recipes that contain at least the selected ingredient
+  app.post("/api/search/", function (req, res) {
     var passed = [];
     req.body.forEach(element => {
       passed.push[element.id];
@@ -26,14 +26,24 @@ module.exports = function(app) {
     db.Recipe.findAll({
       include: [{
         model: Ingredients,
-        where: {id: {[Op.contains]: passed}}
+        where: {
+          id: {
+            [Op.contains]: passed
+          }
+        }
       }]
-    }).then(function(returnedRecipes){
+    }).then(function (returnedRecipes) {
       console.log(returnedRecipes);
+    });
+  });
 
   // Delete a recipe by id
-  app.delete("/api/recipes/:id", function(req, res) {
-    db.recipe.destroy({ where: { id: req.params.id } }).then(function(dbRecipe) {
+  app.delete("/api/recipes/:id", function (req, res) {
+    db.recipe.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbRecipe) {
       res.json(dbRecipe);
       // do we need to delete all lines in join where this recipe appears?
     });

7f44d3cf917c00e77374c4c50e37429b3a4748f3
hilbo <hardimanhk@gmail.com>
2018-11-29 12:25:21 -0500

    deleted console logs in index.js in app/models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,40 +19,6 @@ module.exports = function (app) {
   });
 
   //using the selected ingredients, find all recipes that contain at least the selected ingredient
-<<<<<<< HEAD
-  app.post("/api/search/", function (req, res) {
-    var passed = [];
-    req.body.forEach(element => {
-      passed.push[element.id];
-    });
-    console.log(passed);
-    db.Recipe.findAll({
-      include: [{
-        model: Ingredients,
-        where: {
-          id: {
-            [Op.contains]: passed
-          }
-        }
-      }]
-    }).then(function (returnedRecipes) {
-      console.log(returnedRecipes);
-    });
-  });
-
-  // Delete a recipe by id
-  app.delete("/api/recipes/:id", function (req, res) {
-    db.recipe.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbRecipe) {
-      res.json(dbRecipe);
-      // do we need to delete all lines in join where this recipe appears?
-    });
-  });
-};
-=======
   app.post("/api/search/", function(req, res) {
     console.log(req.body);
     var foo = JSON.parse(req.body.hello);
@@ -71,4 +37,3 @@ module.exports = function (app) {
     })
   })
 };
->>>>>>> 03e7397a41f2fac502e47182b26cc3520bfc41bf

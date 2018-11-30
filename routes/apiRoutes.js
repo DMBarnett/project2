8e385badf1d267920770a4a0ce4fb01d00482eb0
popcorn <40187116+lylekarim@users.noreply.github.com>
2018-11-29 20:51:26 -0500

    adding printjs and twilio

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
+var Op = Sequelize.Op;
 
 module.exports = function (app) {
   // Get all ingredients
@@ -17,26 +19,21 @@ module.exports = function (app) {
   });
 
   //using the selected ingredients, find all recipes that contain at least the selected ingredient
-  app.post("/api/search/", function (req, res) {
-    var passed = [];
-    req.body.forEach(element => {
-      passed.push[element.id];
-    });
-    console.log(passed);
-    db.Recipe.findAll({
+  app.post("/api/search/", function(req, res) {
+    console.log(req.body);
+    var foo = JSON.parse(req.body.hello);
+    console.log(foo);
+    //foo = [1,4];
+    db.recipe
+    .findAll({
       include: [{
-        model: Ingredients,
-        where: { id: { [Op.contains]: passed } }
+        model: db.ingredient,
+        as: "Ingredients",
+        where: {id: { [Op.in]:foo} }
       }]
     }).then(function (returnedRecipes) {
       console.log(returnedRecipes);
-    });
-  });
-      // Delete a recipe by id
-      app.delete("/api/recipes/:id", function (req, res) {
-        db.recipe.destroy({ where: { id: req.params.id } }).then(function (dbRecipe) {
-          res.json(dbRecipe);
-          // do we need to delete all lines in join where this recipe appears?
-        });
-      });
-  };
+      res.json(returnedRecipes);
+    })
+  })
+};

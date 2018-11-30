de8458be7c6e24ec2375057e70a8aefb9909203b
hilbo <hardimanhk@gmail.com>
2018-11-29 21:30:29 -0500

    easy fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,11 +24,7 @@ module.exports = function (app) {
     });
   });
 
-<<<<<<< HEAD
   //using the selected ingredients, find all recipes that contain at least the selected ingredient
-=======
-  // using the selected ingredients, find all recipes that contain at least the selected ingredient
->>>>>>> ac2dabe1d41df1b7239b3031f20bdb2eeb29c5b2
   app.post("/api/search/", function(req, res) {
     console.log(req.body);
     var foo = JSON.parse(req.body.hello);
